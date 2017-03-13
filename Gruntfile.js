bbf0de2546721d3ab918cc04c53ce23b1f5d4a1f
stephanie <s.vallier@atlantis-software.net>
2017-03-13 11:25:50 +0100

    ajout du fichier list.js dans le fichier grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -116,7 +116,8 @@ module.exports = function (grunt) {
           'js/popover.js',
           'js/scrollspy.js',
           'js/tab.js',
-          'js/affix.js'
+          'js/affix.js',
+          'js/list.js'
         ],
         dest: 'dist/js/<%= pkg.name %>.js'
       }
