f60c55cd72d7da9eb11d3bdcce1142e1235db2dd
stephanie147 <lovedesitaliens@gmail.com>
2017-04-18 14:24:18 +0200

    fix datepicker

---
+++ ngx/datepicker-range.component.js
@@ -0,0 +1,35 @@
+import { Component, EventEmitter } from '@angular/core';
+
+export default class datepickerrangeComponent {
+  constructor () {
+    this.startChange = new EventEmitter();
+    this.endChange = new EventEmitter();
+  }
+
+  static get annotations() {
+    return [
+      new Component({
+        selector: 'datepicker-range',
+        template: require('./datepicker-range.html'), 
+        inputs: ['start', 'end'], 
+        outputs: ['startChange', 'endChange']
+      })
+    ];
+  }
+
+  // event start change emit by the datepicker atlantis ui
+  setStart(event){
+    this.start = event.target.value;
+    // emit change value on startChange
+    this.startChange.emit(this.start);
+  }
+
+  // event end change emit by the datepicker atlantis ui
+  setEnd(event){
+    this.end = event.target.value;
+    // emit change value on endChange
+    this.endChange.emit(this.end);
+  }
+}
+
+datepickerrangeComponent.parameters = [];
