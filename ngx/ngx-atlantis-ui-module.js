35ab9fa9c50896d8e16263b66a0e452fbb5b9b9b
stephanie <s.vallier@atlantis-software.net>
2017-04-18 14:17:17 +0200

    importation des composants datepickers renommés

--- ngx/ngx-atlantis-ui-module.js
+++ ngx/ngx-atlantis-ui-module.js
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import pagination from './pagination.component.js';
-import datePickerComponent from './datepicker.component.js';
+import datepickerComponent from './datepicker.component.js';
+import datepickerrangeComponent from './datepicker-range.component.js';
 import selectpickerComponent from './selectpicker.component.js';
 import atlModelDirective from './atlmodel.directive.js';
 import selectpickeroptionComponent from './selectpicker-option.component.js';
@@ -12,7 +13,7 @@ export var ngxAtlUiModule = function() {};
 ngxAtlUiModule.annotations = [
   new NgModule({
     imports: [CommonModule, FormsModule],
-    declarations: [pagination, datePickerComponent, atlModelDirective, selectpickerComponent, selectpickeroptionComponent],
-    exports : [pagination, datePickerComponent, atlModelDirective, selectpickerComponent, selectpickeroptionComponent]
+    declarations: [pagination, datepickerComponent, datepickerrangeComponent, atlModelDirective, selectpickerComponent, selectpickeroptionComponent],
+    exports : [pagination, datepickerComponent, datepickerrangeComponent, atlModelDirective, selectpickerComponent, selectpickeroptionComponent]
   })
 ];
