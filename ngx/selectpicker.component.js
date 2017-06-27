ab6008d8302046275e203c45b37044f491932ff9
stephanie <s.vallier@atlantis-software.net>
2017-06-27 10:53:33 +0200

    select component gestion du ngdocheck et du click à l'exterieur du composant

--- ngx/selectpicker.component.js
+++ ngx/selectpicker.component.js
@@ -1,13 +1,15 @@
-import { Component, ContentChildren, forwardRef, ChangeDetectorRef} from '@angular/core';
+import { Component, ContentChildren, forwardRef, ChangeDetectorRef, IterableDiffers, ElementRef} from '@angular/core';
 import { NG_VALUE_ACCESSOR } from '@angular/forms';
 import selectpickeroptionComponent from './selectpicker-option.component';
 
 export default class selectpickerComponent {
-  constructor (changeDetectorRef) {
+  constructor (changeDetectorRef, differs, elementRef) {
+    this.elementRef = elementRef;
     this.onModelTouched = function() {};
     this.onModelChange = function() {};
     this.cdr = changeDetectorRef;
     this.isOpen = false;
+    this.differ = differs.find([]).create(null);
   }
 	static get annotations() {
 		return [
@@ -22,7 +24,11 @@ export default class selectpickerComponent {
           provide: NG_VALUE_ACCESSOR,
           useExisting: forwardRef(() => selectpickerComponent),
           multi: true
-        }]
+        }], 
+        // need click outsite component
+        host: {
+          '(document:click)': 'handleClick($event)',
+        }
 	  	})
 		];
 	}
@@ -41,19 +47,26 @@ export default class selectpickerComponent {
       this.onModelChange(val);
       var self = this;
       // iniliaze list with selected values
-      if (this.val) {
-        this.SelectedValuesText = [];
-        this.options.toArray().forEach(function(option){
-          var index = self.val.indexOf(option.value);
-          if (index > -1) {
-            option.selected = true;
-            self.SelectedValuesText.push(option.text);
-          }
-        })
-        // if not error
-        this.cdr.detectChanges();
-      }
+      this.updateOptions();
     }
+  }
+
+  updateOptions() {
+    var self = this;
+    if (this.multiple && Array.isArray(this.val)) {
+      this.SelectedValuesText = [];
+      this.options.forEach(function(option){
+        var index = self.val.indexOf(option.value);
+        if (index > -1) {
+          option.selected = true;
+          self.SelectedValuesText.push(option.text);
+        } else {
+          option.selected = false;
+        }
+      })
+    }
+    // if not error
+    this.cdr.detectChanges();
   }
 
   registerOnChange(fn) {
@@ -65,6 +78,31 @@ export default class selectpickerComponent {
 
   Open() {
     this.isOpen = this.isOpen ? false : true;
+  }
+
+  // click outsite component
+  handleClick(event){
+    var clickedComponent = event.target;
+    var inside = false;
+    do {
+      if (clickedComponent === this.elementRef.nativeElement) {
+        inside = true;
+      }
+      clickedComponent = clickedComponent.parentNode;
+    } while (clickedComponent);
+    if(!inside){
+      this.isOpen = false;
+    }
+  }
+  //That check for every selectpicker widget's change.
+  ngDoCheck() {
+    //We use Array.isArray to verify if this.val is an array so we avoid a angular's error with IterableDiffers
+    if (Array.isArray(this.val)) {
+      var changes = this.differ.diff(this.val);
+      if (changes) {
+        this.updateOptions();
+      }
+    }
   }
 
   selectOption(option){
@@ -79,7 +117,6 @@ export default class selectpickerComponent {
       var index = this.val.indexOf(option.value);
       var indexText = this.SelectedValuesText.indexOf(option.text);
       if (index > -1) {
-        //this.val = this.val.splice(index, 1);
         this.val.splice(index, 1);
       }
       if (indexText > -1) {
@@ -87,13 +124,11 @@ export default class selectpickerComponent {
       }
     } else {
       option.selected = true;
-     //this.val = this.val.push(option.value);
      this.val.push(option.value);
      this.SelectedValuesText.push(option.text);
     }
-    //this.onModelChange(this.val.toArray());
     this.onModelChange(this.val.slice(0));
   }
 }
 
-selectpickerComponent.parameters = [ChangeDetectorRef];
+selectpickerComponent.parameters = [ChangeDetectorRef, IterableDiffers, ElementRef];
