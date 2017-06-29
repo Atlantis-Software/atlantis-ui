f6a093224dd02eb44b484326f64cd18ee1bf7f90
stephanie <s.vallier@atlantis-software.net>
2017-06-29 17:54:51 +0200

    select option component traitement est fait dans la fonction ngAfterViewInit pour que les valeurs bindées fonctionnent

--- ngx/selectpicker-option.component.js
+++ ngx/selectpicker-option.component.js
@@ -3,6 +3,7 @@ import { Component, ElementRef} from '@angular/core';
 export default class selectpickeroptionComponent {
   constructor (elementRef) {
     this.elementRef = elementRef;
+    // by default value is not selected
     this.selected = false;
   }
 	static get annotations() {
@@ -15,12 +16,14 @@ export default class selectpickeroptionComponent {
 		];
 	}
 
+  // for the bind value work we must do it in the ngAfterViewInit function 
+  // baecause wa can't not acces to bind value before in the Lifecycle Hooks
   ngAfterViewInit() {
-    if (typeof this.val != 'undefined') {
-      this.value = this.elementRef.nativeElement.innerText.trim();
+    this.text = this.elementRef.nativeElement.innerHTML.trim();
+    // if value is not defined it take value of the text
+    if (typeof this.value === 'undefined') {
+      this.value = this.elementRef.nativeElement.innerHTML.trim();
     }
-
-    this.text = this.elementRef.nativeElement.innerText.trim();
   }
 }
 
