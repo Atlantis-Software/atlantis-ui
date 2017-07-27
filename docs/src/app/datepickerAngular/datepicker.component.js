db6d2dfb77732338cfc23a24a7911056dfd02f92
stephanie <s.vallier@atlantis-software.net>
2017-07-27 10:38:07 +0200

    doc datepicker

--- docs/src/app/datepickerAngular/datepicker.component.js
+++ docs/src/app/datepickerAngular/datepicker.component.js
@@ -10,20 +10,13 @@ export default  class DatepickerAngularComponent {
   }
   constructor(){
     this.DatepickerRangeHtml = `
-    <datepicker-range [(start)]="start1" [(end)]="end1"></datepicker-range>`
+    <datepicker-range [(start)]="start1" [(end)]="end1" [numberOfMonths]="4"></datepicker-range>`
 
     this.DatepickerSimpleHtml = `
-    <datepicker [(ngModel)]="start3"></datepicker>`
-
-
-
-    this.Display = "Example";
+    <datepicker [(ngModel)]="valueDatePicker" [numberOfMonths]="5"></datepicker>`
 
     this.start1 = "2017-05-01";
     this.end1 = "2017-05-22";
-    this.start2 = "2017-05-01";
-    this.end2 = "2017-05-22";
-    this.start4 = "2017-05-01";
   }
 }
 
