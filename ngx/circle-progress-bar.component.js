1969ae04a84b2105874d2aabff4b488570506866
vincent <v.travers@atlantis-software.net>
2017-11-15 11:39:54 +0100

    update circle-progress-bar

--- ngx/circle-progress-bar.component.js
+++ ngx/circle-progress-bar.component.js
@@ -6,8 +6,8 @@ export default class circleProgessBarComponent {
       new Component({
         selector: 'circle-progress-bar, circle-progress-bar-sm, circle-progress-bar-lg',
         template: `
-        <div id="cont" [attr.data-percent]="value" class="circle-progress-bar circle-progress-bar-default">
-          <svg id="svg" version="1.1" xmlns="http://www.w3.org/2000/svg">
+        <div [attr.data-percent]="value" class="circle-progress-bar circle-progress-bar-default">
+          <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
             <circle class="background-bar" fill="transparent" stroke-dasharray="565.48" stroke-dashoffset="0"></circle>
             <circle class="bar" fill="transparent" stroke-dasharray="565.48" stroke-dashoffset="0"></circle>
           </svg>
@@ -23,33 +23,27 @@ export default class circleProgessBarComponent {
 
   ngAfterViewInit() {
     this.circle = this.elementRef.nativeElement.getElementsByClassName("bar")[0];
+    this.circleBackground = this.elementRef.nativeElement.getElementsByClassName("background-bar")[0];
     this.content = this.elementRef.nativeElement.getElementsByClassName("circle-progress-bar")[0];
-    this.tag = this.elementRef.nativeElement.tagName;
     this.style = this.elementRef.nativeElement.classList;
-    //Define the correct attribute for svg with tagName
-    switch (this.tag) {
-      case "CIRCLE-PROGRESS-BAR-SM":
-        this.circle.setAttribute("r", 60);
-        this.circle.setAttribute("cx", 70);
-        this.circle.setAttribute("cy", 70);
-        this.content.classList.add("circle-progress-bar-sm");
-        break;
-      case "CIRCLE-PROGRESS-BAR-LG":
-        this.circle.setAttribute("r", 76);
-        this.circle.setAttribute("cx", 86);
-        this.circle.setAttribute("cy", 86);
-        this.content.classList.add("circle-progress-bar-lg");
-        break;
-      default:
-        this.circle.setAttribute("r", 64);
-        this.circle.setAttribute("cx", 74);
-        this.circle.setAttribute("cy", 74);
-        this.content.classList.add("circle-progress-bar-md");
-    }
-    var self = this;
-    this.style.forEach(function(style) {
-      self.content.classList.add(style);
+    this.tag = this.elementRef.nativeElement.tagName;
+
+    this.content.classList.add(this.tag.toLowerCase());
+
+    var contentWidth = this.content.offsetWidth;
+    this.c = contentWidth / 2;
+    this.r = this.c - 10;
+    this.circle.setAttribute("r", this.r);
+    this.circle.setAttribute("cx", this.c);
+    this.circle.setAttribute("cy", this.c);
+    this.circleBackground.setAttribute("r", this.r);
+    this.circleBackground.setAttribute("cx", this.c);
+    this.circleBackground.setAttribute("cy", this.c);
+
+    this.style.forEach((style)=>{
+      this.content.classList.add(style);
     });
+
     if (typeof this.value !== "number" || this.value === "") {
       this.value = 0;
     }
@@ -59,8 +53,8 @@ export default class circleProgessBarComponent {
     } else if (this.value < 0) {
       this.value = 0;
     }
-    var r = this.circle.getAttribute("r");
-    this.circle.style.strokeDasharray = Math.PI * (r * 2);
+    this.circle.style.strokeDasharray = Math.PI * (this.r * 2);
+    this.circleBackground.style.strokeDasharray = Math.PI * (this.r *2);
     this.updateBar();
   }
 
@@ -81,9 +75,7 @@ export default class circleProgessBarComponent {
 
   //Update the progression on the bar
   updateBar() {
-    var r = this.circle.getAttribute("r");
-    var c = Math.PI * (r * 2);
-    var percent = ((100 - this.value) / 100) * c;
+    var percent = ((100 - this.value) / 100) * (Math.PI * (this.r*2));
 
     this.circle.style.strokeDashoffset = percent;
 
