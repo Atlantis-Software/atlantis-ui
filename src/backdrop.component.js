90d3a2aadca40c4674649adb1beb89d729a421b6
vincent <v.travers@atlantis-software.net>
2018-01-15 11:12:37 +0100

    add prefix to backdrop

--- src/backdrop.component.js
+++ src/backdrop.component.js
@@ -4,7 +4,7 @@ export default class backdropComponent {
   static get annotations() {
     return [
       new Component({
-        selector: 'backdrop',
+        selector: 'atlui-backdrop',
         template: '',
         host: {
           "class": "modal-backdrop fade in",
