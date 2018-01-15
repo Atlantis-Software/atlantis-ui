9fa344bda2025fdf4c62d48bc844282ce3c6b299
vincent <v.travers@atlantis-software.net>
2018-01-15 16:35:01 +0100

    update home

--- docs/src/app/home/home.component.js
+++ docs/src/app/home/home.component.js
@@ -13,53 +13,6 @@ export default class HomeComponent {
     ];
   }
   constructor(){
-
-    this.importHtml=`
-    import {ngxAtlUiModule} from 'path/atlantis-ui-ngx' or 'aliasWithWebpack'
-
-    @NgModule({
-      ...
-      import: [ngxAtlUiModule.forRoot(object)],
-      ...
-    })`;
-    this.gridParameters = `
-    var types = [
-      {
-        type: "date",
-        alignment: "right",
-        pipes: [DatePipe],
-        optionsPipe: ['shortDate'],
-        transformation: function(val) {
-          if (moment(val).isValid()) {
-            return moment(val).toString();
-          } else {
-            return "invalid value";
-          }
-        }
-      },
-      {
-        type: 'number',
-        transformation: function(val) {
-          if (!isNaN(val)) {
-            return val;
-          } else {
-            return "invalid value";
-          }
-        }
-      },
-      {
-        type: 'boolean',
-        transformation: function(val) {
-          if (typeof val === void 0 || val === null) {
-            return val;
-          }
-          if (!val || val == '0' || val == 'false') {
-            return 0;
-          }
-          return 1;
-        }
-      },
-    ];`;
   }
 
 }
