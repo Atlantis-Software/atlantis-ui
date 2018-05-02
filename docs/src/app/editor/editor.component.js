fc518e012051167541e1773f1b2eace361583618
vincent <v.travers@atlantis-software.net>
2018-05-02 15:02:15 +0200

    add begin doc for editor

--- docs/src/app/editor/editor.component.js
+++ docs/src/app/editor/editor.component.js
@@ -9,6 +9,17 @@ export default class EditorComponent {
     ];
   }
   constructor(){
+    this.toolbar = [
+      ["undo", "redo"],
+      ["selectAll"],
+      ["bold", "italic", "underline", "strikethrough", "removeFormat"],
+      ["insertOrderedList", "insertUnorderedList", "outdent", "indent", "blockquote","justifyLeft", "justifyCenter", "justifyRight", "justifyFull"],
+      ['insertImage', 'createLink', "unlink"],
+      ['fontSize'],
+      ['format'],
+      ['fontName'],
+      ['foreColor', 'hiliteColor']
+    ];
   }
 }
 
